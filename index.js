7b56140411192df1c0dc1de63e8b1bd40b272426
Michael Moore <michael.moore@contrastsecurity.com>
2020-11-27 14:11:40 -0500

    use heapdump lib and reduce grid dimensions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const heapdump = require('heapdump')
 const maze = require('@miketmoore/maze-generator')
-const size = 1016
+const size = 500
 const grid = maze.mazeGenerator({ rows: size, columns: size }, 'iterative')
+heapdump.writeSnapshot('first-snapshot.heapsnapshot')
